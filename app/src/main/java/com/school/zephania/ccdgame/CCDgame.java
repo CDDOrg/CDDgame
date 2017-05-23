3ba0573a6c824e78e8f3f83a5a9dea0fde0cab27
zephania <zephaniacn@outlook.com>
2017-05-23 12:58:54 +0800

    build player and other file

---
+++ app/src/main/java/com/school/zephania/ccdgame/CCDgame.java
@@ -0,0 +1,35 @@
+package com.school.zephania.ccdgame;
+
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.school.zephania.ccdgame.view.GameView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class CCDgame extends Activity {
+    private GameView gameView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        gameView=new GameView(this);
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/school/zephania/ccdgame/model/player.java
@@ -0,0 +1,8 @@
+package com.school.zephania.ccdgame.model;
+
+/**
+ * Created by zephania on 17-5-23.
+ */
+
+public class player {
+}
