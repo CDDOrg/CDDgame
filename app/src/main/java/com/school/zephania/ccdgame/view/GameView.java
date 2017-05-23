b0bda502cf22e92f2f9ca16a556a6f10e1a593f7
zephania <zephaniacn@outlook.com>
2017-05-23 16:29:41 +0800

    add god to gameview

--- app/src/main/java/com/school/zephania/ccdgame/view/GameView.java
+++ app/src/main/java/com/school/zephania/ccdgame/view/GameView.java
@@ -8,16 +8,18 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import com.school.zephania.ccdgame.model.God;
 
 /**
  * Created by zephania on 17-5-23.
  */
 
 public class GameView extends View implements View.OnTouchListener {
-
+    private God god;
     public GameView(Context context){
         super(context);
         this.setOnTouchListener(this);
+        God god = new God();
     }
 
 
