949d6098ef87da58e9cd128a8704cd71f5cd4e88
Zephania Liu <610518684@qq.com>
2017-05-26 17:10:56 +0900

    display name

--- app/src/main/java/com/school/zephania/ccdgame/model/Player.java
+++ app/src/main/java/com/school/zephania/ccdgame/model/Player.java
@@ -3,12 +3,14 @@ package com.school.zephania.ccdgame.model;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 
 import com.school.zephania.ccdgame.CCDgame;
 import com.school.zephania.ccdgame.R;
 
-import static com.school.zephania.ccdgame.R.drawable.headbinke;
+
 
 /**
  * Created by zephania on 17-5-23.
@@ -18,6 +20,7 @@ public class Player {
     private Bitmap head;
     int x;
     int y;
+    String name= "binke";
 
     public Player(CCDgame ccd,int x,int y)
     {
@@ -35,6 +38,15 @@ public class Player {
         Rect src = new Rect(80,0,head.getWidth(),head.getHeight());
         Rect dst = new Rect(x,y,x+(head.getWidth()-85)*3/5,y+head.getHeight()*3/5);
         canvas.drawBitmap(head,src,dst,null);
+
+        Paint textpaint=new Paint();
+        textpaint.setColor(Color.BLACK);
+        textpaint.setStyle(Paint.Style.FILL);
+        textpaint.setTextSize(40);
+        textpaint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(name,dst.centerX(),dst.bottom-15,textpaint);
+
+
     }
 
 }
